8ec703fd1962ecdf2429630bedd5ae3329cc0932
PRESTEBAN <145607969+PRESTEBAN@users.noreply.github.com>
2025-04-09 08:47:01 -0500

    Add files via upload
    c

--- src/App.java
+++ src/App.java
@@ -14,22 +14,32 @@ public class App {
             System.out.println("1. Suma de números consecutivos por recursividad");
             System.out.println("2. Petencia de un número");
             System.out.println("3. Suma de dígitos de un número ");
-            System.out.println("4. Para salir ");
+            System.out.println("4. Fibonacci");
+            System.out.println("5. Para salir ");
 
             byte opcion = scanner.nextByte();
 
-            if(opcion==4){
+            if(opcion==5){
              System.out.println("Saliendo!!");
              break;
             }
 
             switch (opcion) {
             case 1:
-                System.out.println("Has ingresado la opción 1: ");
-                System.out.println("Ingresa hasta que número deseas que se realice la suma: ");
+               /*  System.out.println("Has ingresado la opción 1: ");
+                System.out.println("Ingresa hasta que número deseas que se real ice la suma: ");
                 int num = scanner.nextInt();
                 int resultado = ej.sumaRecursiva(1, num);
-                System.out.println("La suma es: " + resultado);
+                System.out.println("La suma es: " + resultado);*/
+
+                System.out.println("Ingresa hasta que número deseas que se realice la suma: ");
+                int num = scanner.nextInt();
+
+                int resultado = ej.sumConsecutivos(num);
+
+                System.out.println(resultado);
+                
+
                 break;
             case 2:
                 System.out.println("Has ingresado la opción 2: ");
@@ -43,9 +53,15 @@ public class App {
             case 3:
                 System.out.println("Has ingresado la opción 3: ");
                 System.out.println("Ingresa el número cuyos dígitos deseas sumar: ");
-                int numero = scanner.nextInt();
-                int resultadoSuma = ej.sumaDigitos(numero);
-                System.out.println("La suma de los dígitos de " + numero + " es: " + resultadoSuma);
+                int numer = scanner.nextInt();
+                int resultadoSuma = ej.sumaDigitos(numer);
+                System.out.println("La suma de los dígitos de " + numer + " es: " + resultadoSuma);
+                break;
+            case  4:
+                System.out.println("Has ingresado la opción 4");
+                int n = scanner.nextInt();
+                int resultado4 = ej.fibonacci(n);
+                System.out.println(resultado4);
                 break;
             }
             
--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -1,6 +1,6 @@
 public class Ejercicios {
 
-
+    public int llamadasRecursiva =1;
     public int sumaRecursiva(int numero, int limite){
         if (numero > limite) {
             return 0;
@@ -8,6 +8,35 @@ public class Ejercicios {
             System.out.println(numero + " + ");
             return numero + sumaRecursiva(numero+1, limite);
         }
+    }
+
+    public int sumConsecutivos(int n){
+
+        int idActual = llamadasRecursiva++;
+
+        System.out.println("Llamada a " + idActual +
+                            " con numero n = " + n );
+
+        if (n==1) {
+            System.out.println("Llamada a" + idActual +
+                                " caso base alcanzado ");
+            return 1;
+        }
+        //return n + sumConsecutivos(n);
+        System.out.println("Llamada a " + idActual +
+                            " Lamando recursivamente con n =" + (n-1));
+
+        int resultadoParcial = sumConsecutivos(n-1);
+
+        int resultado = n + resultadoParcial;
+        System.out.println("Llamada a " + idActual +
+         " sumando  "+ n + " al resultado de " + resultadoParcial +
+        " = " + resultado) ;
+
+        System.out.println("Llamada a " + idActual +
+                " Retorno " + resultado);
+
+        return resultado;
     }
 
     public int potenciaRecursiva(int base, int exponente, int aux){
@@ -20,15 +49,24 @@ public class Ejercicios {
         }
     }
 
-    public int sumaDigitos(int numero) {   //12
-        if (numero < 10) { // no cumple
+    public int sumaDigitos(int numero) {   
+        if (numero < 10) {
             return numero;
         } else {
           
-            int ultimoDigito = numero % 10; //2
-            int resto = numero / 10; //2
+            int ultimoDigito = numero % 10; 
+            int resto = numero / 10; 
         
-            return ultimoDigito + sumaDigitos(resto); // 2 + 1
+            return ultimoDigito + sumaDigitos(resto); 
         }
+    }
+
+    public int fibonacci(int n){
+        // Caso base: los dos primeros números de Fibonacci son 0 y 1
+        if (n==0 || n==1) {
+            return n;
+        }
+        // Caso recursivo: sumar los dos números anteriores en la secuencia 
+        return fibonacci(n-1) + fibonacci(n-2);
     }
 }
